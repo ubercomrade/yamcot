4b3fd4130a13a600e2484129df28e01b381a3ce3
Anton Tsukanov <ubercomrade@gmail.com>
2026-02-03 11:33:41 +0700

    fix

---
+++ examples/run.ps1
@@ -0,0 +1,86 @@
+yamcot motif ./bamm ./pif4.meme `
+--model1-type bamm `
+--model2-type pwm `
+--motif-perm 500 `
+--motif-metric co
+
+yamcot motif ./gata2 ./gata4 `
+--model1-type bamm `
+--model2-type bamm `
+--motif-perm 1000 `
+--motif-metric cj
+
+yamcot motif ./sitega_stat6.mat ./pif4.meme `
+--model1-type sitega `
+--model2-type pwm `
+--motif-perm 500 `
+--motif-metric co
+
+yamcot tomtom-like ./sitega_gata2.mat ./pif4.meme `
+--model1-type sitega `
+--model2-type pwm `
+--metric ed `
+--permutations 500
+
+yamcot tomtom-like ./pif4.meme ./pif4.meme `
+--model1-type pwm `
+--model2-type pwm `
+--metric ed `
+--permutations 10000 `
+--permute-rows
+
+yamcot tomtom-like ./sitega_stat6.mat ./pif4.meme `
+--model1-type sitega `
+--model2-type pwm `
+--metric pcc `
+--permutations 1000 `
+--pfm-mode 
+
+yamcot sequence ./sitega.mat ./pif4.meme `
+--model1-type sitega `
+--model2-type pwm `
+--metric cj `
+--perm 1000
+
+yamcot tomtom-like ./sitega_stat6.mat ./sitega_gata2.mat `
+--model1-type sitega `
+--model2-type sitega `
+--metric pcc `
+--permutations 10000 `
+--permute-rows `
+--pfm-mode
+
+yamcot tomtom-like ./sitega_stat6.mat ./sitega_gata2.mat `
+--model1-type sitega `
+--model2-type sitega `
+--metric ed `
+--permutations 10000 `
+--permute-rows `
+--pfm-mode
+
+yamcot tomtom-like ./sitega_stat6.mat ./sitega_stat6.mat `
+--model1-type sitega `
+--model2-type sitega `
+--metric ed `
+--permutations 10000 `
+--permute-rows `
+--pfm-mode
+
+yamcot tomtom-like ./gata2.meme ./sitega_gata2.mat `
+--model1-type pwm `
+--model2-type sitega `
+--metric ed `
+--permutations 1000 `
+--pfm-mode
+
+yamcot tomtom-like ./gata2 ./gata4 `
+--model1-type bamm `
+--model2-type bamm `
+--permutations 1000 `
+--metric ed -v 
+
+yamcot score ./scores_1.fasta ./scores_2.fasta
+
+yamcot motali ./gata2.meme ./sitega_gata2.mat `
+--model1-type pwm `
+--model2-type sitega
